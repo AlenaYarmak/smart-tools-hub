84373287487f8ee6bfd06532c8627e6ff995fdf6
Alena <olena.yarmak.mail@gmail.com>
2024-08-28 21:51:38 +0200

    change arrow in accordion

--- src/components/AccElement.js
+++ src/components/AccElement.js
@@ -4,8 +4,8 @@ import AccordionContext from 'react-bootstrap/AccordionContext';
 import { useAccordionButton } from 'react-bootstrap/AccordionButton';
 import Card from 'react-bootstrap/Card';
 
-const PINK = 'rgba(255, 192, 203, 0.6)';
-const BLUE = 'rgba(0, 0, 255, 0.6)';
+const openedPosition = 'rotate(0)';
+const defaultPosition = 'rotate(-90deg)';
 
 function ContextAwareToggle({ children, eventKey, callback, setActiveKey }) {
     const { activeEventKey } = useContext(AccordionContext);
@@ -14,28 +14,41 @@ function ContextAwareToggle({ children, eventKey, callback, setActiveKey }) {
         eventKey,
         () => {
             if (callback) callback(eventKey);
-            setActiveKey(activeEventKey === eventKey ? null : eventKey);  // Toggle the active key
+            setActiveKey(prevActiveKey => 
+                prevActiveKey.includes(eventKey) 
+                    ? prevActiveKey.filter(key => key !== eventKey) 
+                    : [...prevActiveKey, eventKey]
+            );
         }
     );
 
-    const isCurrentEventKey = activeEventKey === eventKey;
+    const isCurrentEventKey = activeEventKey.includes(eventKey);
 
     return (
-        <button
-            type="button"
-            style={{ backgroundColor: isCurrentEventKey ? PINK : BLUE }}
-            onClick={decoratedOnClick}
-        >
-            {children}
-        </button>
+        <div onClick={decoratedOnClick}>
+            <svg
+                style={{
+                    transition: '0.3s',
+                    transform: isCurrentEventKey ? openedPosition : defaultPosition
+                }}
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-caret-down"
+                viewBox="0 0 16 16"
+            >
+                <path d="M3.204 5h9.592L8 10.481zm-.753.659 4.796 5.48a1 1 0 0 0 1.506 0l4.796-5.48c.566-.647.106-1.659-.753-1.659H3.204a1 1 0 0 0-.753 1.659" />
+            </svg>
+        </div>
     );
 }
 
 const AccElement = () => {
-    const [activeKey, setActiveKey] = useState(null);  // No accordion is open initially
+    const [activeKey, setActiveKey] = useState([]);  // Multiple accordion items can be open
 
     return (
-        <Accordion activeKey={activeKey} onSelect={(eventKey) => setActiveKey(eventKey)}>
+        <Accordion alwaysOpen={true} activeKey={activeKey}>
             <Card>
                 <Card.Header>
                     <ContextAwareToggle eventKey="0" setActiveKey={setActiveKey}>Click me!</ContextAwareToggle>
