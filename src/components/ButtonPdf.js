afee8cb335157623f93a5ab7606cd48c97044dd6
Alena <olena.yarmak.mail@gmail.com>
2024-09-03 00:03:31 +0200

    add generatePdf function

--- src/components/ButtonPdf.js
+++ src/components/ButtonPdf.js
@@ -1,8 +1,36 @@
 import React from "react";
+import { jsPDF } from "jspdf";
+import mockData from "../assets/data/mockData.json";
 
 const ButtonPdf = () => {
+
+    const generatePdf = () => {
+        const file = new jsPDF();
+        
+        file.setFontSize(18);
+        file.text("Mock Data", 20, 20);
+
+        file.setFontSize(12);
+        file.text("Title", 20, 40);
+        file.text("Subtitle", 50, 40);
+        file.text("Description", 120, 40);
+
+        let yPosition = 50;
+        mockData.forEach((item) => {
+            file.text(item.title, 20, yPosition);
+            file.text(item.subtitle, 50, yPosition);
+            file.text(item.description, 120, yPosition);
+            yPosition += 10;
+        });
+
+        file.save("mock-data.pdf");
+    }
+    
     return(
-        <button type="button" class="button__fs btn btn-outline-primary btn-lg py-3 px-4">
+        <button 
+            type="button" 
+            class="button__fs btn btn-outline-primary btn-lg py-3 px-4"
+            onClick={generatePdf}>
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download me-4" viewBox="0 0 16 16">
                 <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5"/>
                 <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708z"/>
