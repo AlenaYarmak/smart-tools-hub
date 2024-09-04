ad9248c9b1b79d543139b00e6d86f1d365de3312
Alena <olena.yarmak.mail@gmail.com>
2024-09-04 22:52:38 +0200

    add docx file-saver / add doc saver button

--- src/components/ButtonPdf.js
+++ src/components/ButtonPdf.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { jsPDF } from "jspdf";
+import { Document, Packer, Paragraph, TextRun } from "docx";
+import { saveAs } from "file-saver";
 import mockData from "../assets/data/mockData.json";
 
 const ButtonPdf = () => {
@@ -25,9 +27,44 @@ const ButtonPdf = () => {
 
         file.save("mock-data.pdf");
     }
+
+    const generateDocx = () => {
+        const doc = new Document({
+          sections: [
+            {
+              children: mockData.map(
+                (item) =>
+                  new Paragraph({
+                    children: [
+                      new TextRun({
+                        text: item.title,
+                        bold: true,
+                        size: 24,
+                      }),
+                      new TextRun({
+                        text: `\n${item.subtitle}`,
+                        italics: true,
+                        size: 20,
+                      }),
+                      new TextRun({
+                        text: `\n${item.description}\n`,
+                        size: 16,
+                      }),
+                    ],
+                  })
+              ),
+            },
+          ],
+        });
+    
+        Packer.toBlob(doc).then((blob) => {
+          saveAs(blob, "mock-data.docx");
+        });
+      };
     
     return(
-        <button 
+        <div class="btn-group" role="group">
+            <button 
             type="button" 
             class="button__fs btn btn-outline-primary btn-lg py-3 px-4"
             onClick={generatePdf}>
@@ -37,6 +74,17 @@ const ButtonPdf = () => {
             </svg>
             PDF
         </button>
+        <button 
+            type="button" 
+            class="button__fs btn btn-outline-primary btn-lg py-3 px-4"
+            onClick={generateDocx}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download me-4" viewBox="0 0 16 16">
+                <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5"/>
+                <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708z"/>
+            </svg>
+            DOC
+        </button>
+        </div>
     )
 }
 
