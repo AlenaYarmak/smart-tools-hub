6b535601a72ed69f4bf31a7489b91627d990b261
Alena <olena.yarmak.mail@gmail.com>
2024-09-10 19:05:01 +0200

    save opened items (AccElement) in localStorage

--- src/views/MianPage.js
+++ src/views/MianPage.js
@@ -4,21 +4,24 @@ import mockData from '../assets/data/mockData.json';
 import Header from '../components/Header';
 
 const MainPage = () => {
-    return (
-        <>
-            <Header />
-            <div className='container'>
-                <div className='container__wrap m-top'>
-                {mockData.map(item => (
-                    <AccElement 
-                        subtitle={item.subtitle}
-                        description={item.description}
-                        title={item.title}/>
-                ))}
-                </div>
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <div className="container__wrap m-top">
+          {mockData.map((item, index) => (
+            <AccElement
+              key={index}
+              index={index}
+              subtitle={item.subtitle}
+              description={item.description}
+              title={item.title}
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default MainPage;
