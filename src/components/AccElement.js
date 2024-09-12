deb51a3858b599923972054c62722c0704b0a2fb
Alena <olena.yarmak.mail@gmail.com>
2024-09-12 12:47:37 +0200

    add anchors navigations

--- src/components/AccElement.js
+++ src/components/AccElement.js
@@ -1,4 +1,5 @@
-import React, { useState, useContext, useEffect } from 'react';
+// AccElement.js
+import React, { useState, useContext, useEffect, forwardRef } from 'react';
 import Accordion from 'react-bootstrap/Accordion';
 import AccordionContext from 'react-bootstrap/AccordionContext';
 import { useAccordionButton } from 'react-bootstrap/AccordionButton';
@@ -9,77 +10,80 @@ const openedPosition = 'rotate(0)';
 const defaultPosition = 'rotate(-90deg)';
 
 function ContextAwareToggle({ children, eventKey, callback, setActiveKey, title }) {
-    const { activeEventKey } = useContext(AccordionContext);
+  const { activeEventKey } = useContext(AccordionContext);
 
-    const decoratedOnClick = useAccordionButton(
-        eventKey,
-        () => {
-            if (callback) callback(eventKey);
-            setActiveKey(prevActiveKey => 
-                prevActiveKey.includes(eventKey) 
-                    ? prevActiveKey.filter(key => key !== eventKey) 
-                    : [...prevActiveKey, eventKey]
-            );
-        }
-    );
+  const decoratedOnClick = useAccordionButton(
+    eventKey,
+    () => {
+      if (callback) callback(eventKey);
+      setActiveKey((prevActiveKey) =>
+        prevActiveKey.includes(eventKey)
+          ? prevActiveKey.filter((key) => key !== eventKey)
+          : [...prevActiveKey, eventKey]
+      );
+    }
+  );
 
-    const isCurrentEventKey = activeEventKey.includes(eventKey);
+  const isCurrentEventKey = activeEventKey.includes(eventKey);
 
-    return (
-        <div onClick={decoratedOnClick} className='d-flex align-items-center'>
-            <svg
-                style={{
-                    transition: '0.3s',
-                    transform: isCurrentEventKey ? openedPosition : defaultPosition
-                }}
-                xmlns='http://www.w3.org/2000/svg'
-                width='16'
-                height='16'
-                fill='currentColor'
-                className='bi bi-caret-down'
-                viewBox='0 0 16 16'
-            >
-                <path d='M3.204 5h9.592L8 10.481zm-.753.659 4.796 5.48a1 1 0 0 0 1.506 0l4.796-5.48c.566-.647.106-1.659-.753-1.659H3.204a1 1 0 0 0-.753 1.659' />
-            </svg>
-            <p className='m-0 ps-2 title--fs'>{title}</p>
-        </div>
-    );
+  return (
+    <div onClick={decoratedOnClick} className='d-flex align-items-center'>
+      <svg
+        style={{
+          transition: '0.3s',
+          transform: isCurrentEventKey ? openedPosition : defaultPosition,
+        }}
+        xmlns='http://www.w3.org/2000/svg'
+        width='16'
+        height='16'
+        fill='currentColor'
+        className='bi bi-caret-down'
+        viewBox='0 0 16 16'
+      >
+        <path d='M3.204 5h9.592L8 10.481zm-.753.659 4.796 5.48a1 1 0 0 0 1.506 0l4.796-5.48c.566-.647.106-1.659-.753-1.659H3.204a1 1 0 0 0-.753 1.659' />
+      </svg>
+      <p className='m-0 ps-2 title--fs'>{title}</p>
+    </div>
+  );
 }
 
-const AccElement = ({ subtitle, description, title, index }) => {
-   const accordionArrayLength = mockData.length;
-   function generateStringArray(n) {
+// AccElement Component with forwardRef to receive the ref
+const AccElement = forwardRef(({ subtitle, description, title, index }, ref) => {
+  const accordionArrayLength = mockData.length;
+
+  // Function to generate array of strings
+  function generateStringArray(n) {
     return Array.from({ length: n }, (_, i) => i.toString());
   }
-  
-   const sessionStorageKey = `accordionState-${index}`;
 
-   const getInitialState = () => {
-     const savedState = sessionStorage.getItem(sessionStorageKey);
-     return savedState ? JSON.parse(savedState) : generateStringArray(accordionArrayLength);
-   };
- 
-   const [activeKey, setActiveKey] = useState(getInitialState);
- 
-   useEffect(() => {
+  const sessionStorageKey = `accordionState-${index}`;
+
+  const getInitialState = () => {
+    const savedState = sessionStorage.getItem(sessionStorageKey);
+    return savedState ? JSON.parse(savedState) : generateStringArray(accordionArrayLength);
+  };
+
+  const [activeKey, setActiveKey] = useState(getInitialState);
+
+  useEffect(() => {
     sessionStorage.setItem(sessionStorageKey, JSON.stringify(activeKey));
-   }, [activeKey, sessionStorageKey]);
+  }, [activeKey, sessionStorageKey]);
 
-    return (
-        <Accordion className='mb-5' alwaysOpen={true} activeKey={activeKey} >
-            <Card className='py-3 border-0 rounded-5 background-color'>
-                <Card.Header className='border-0 p-2 mx-auto background-color'  style={{width: '98%'}}>
-                    <ContextAwareToggle eventKey='0' setActiveKey={setActiveKey} title={title}></ContextAwareToggle>
-                </Card.Header>
-                <Accordion.Collapse eventKey='0' className='rounded-5'>
-                    <Card.Body className='px-3 py-4 mx-3 my-2 rounded-5 secondary-color'>
-                        <Card.Subtitle className='subtitle subtitle--spacing'>{subtitle}</Card.Subtitle>
-                        <Card.Text className='text'>{description}</Card.Text>
-                    </Card.Body>
-                </Accordion.Collapse>
-            </Card>
-        </Accordion>
-    );
-}
+  return (
+    <Accordion ref={ref} className='mb-5' alwaysOpen={true} activeKey={activeKey}>
+      <Card className='py-3 border-0 rounded-5 background-color'>
+        <Card.Header className='border-0 p-2 mx-auto background-color' style={{ width: '98%' }}>
+          <ContextAwareToggle eventKey='0' setActiveKey={setActiveKey} title={title} />
+        </Card.Header>
+        <Accordion.Collapse eventKey='0' className='rounded-5'>
+          <Card.Body className='px-3 py-4 mx-3 my-2 rounded-5 secondary-color'>
+            <Card.Subtitle className='subtitle subtitle--spacing'>{subtitle}</Card.Subtitle>
+            <Card.Text className='text'>{description}</Card.Text>
+          </Card.Body>
+        </Accordion.Collapse>
+      </Card>
+    </Accordion>
+  );
+});
 
 export default AccElement;
--- src/views/MianPage.js
+++ src/views/MianPage.js
@@ -1,25 +1,35 @@
-import React from 'react';
-import AccElement from '../components/AccElement';
+import React, { useRef } from 'react';
+import AccElement from '../components/AccElement'; // Make sure this path is correct
 import mockData from '../assets/data/mockData.json';
 import Header from '../components/Header';
 import MenuItem from '../components/MenuItem';
 
 const MainPage = () => {
+  const refs = useRef(mockData.map(() => React.createRef()));
+  const handleScrollTo = (index) => {
+    if (refs.current[index] && refs.current[index].current) {
+      refs.current[index].current.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
   return (
     <>
       <Header />
       <div className='container'>
         <div className='d-flex flex-column menu__wrapper'>
-            {mockData.map((item, index) => (
-                <MenuItem 
-                    key={index}
-                    text={item.title}/>
-            ))}
+          {mockData.map((item, index) => (
+            <MenuItem 
+              onClick={() => handleScrollTo(index)} 
+              key={index} 
+              text={item.title} 
+            />
+          ))}
         </div>
         <div className='container__wrap m-top'>
           {mockData.map((item, index) => (
             <AccElement
               key={index}
+              ref={refs.current[index]}
               index={index}
               subtitle={item.subtitle}
               description={item.description}
