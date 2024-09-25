f0c8823a04acfa080381067d6d3094251975e701
Alena <olena.yarmak.mail@gmail.com>
2024-09-25 14:20:13 +0200

    download react routing

---
+++ src/views/AboutPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../components/Header';
+
+const AboutPage = () => {
+    return (
+        <>
+            <Header />
+            <p className='text-uppercase'>About page</p>
+        </>
+    )
+}
+
+export default AboutPage;
