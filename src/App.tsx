d0e017ac4da8955f7ffd7d9d89b80d80764d9cbf
Alena <olena.yarmak.mail@gmail.com>
2024-12-23 20:24:23 +0100

    add new route

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import MainPage from './views/MianPage';
 import AboutPage from './views/AboutPage';
+import DescribePage from './views/DescribePage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path='/' element={<AboutPage />} />
         <Route path='/download' element={<MainPage />} />
+        <Route path='/discover-tools' element={<DescribePage />} />
       </Routes>
     </div>
   );
---
+++ src/views/DescribePage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import HeaderBlock from '../components/HeaderBlock';
+
+const DescribePage = () => {
+    return(
+        <>
+            <HeaderBlock />
+            <div className='container'>
+                <h2 className='title--fs'>Сheck out our tools</h2>
+            </div>
+        </>
+    )
+}
+
+export default DescribePage;
